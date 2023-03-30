51f894e3e5be1969b378e9d2709816fdee30bf0a
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-03-30 15:51:07 -0300

    Create FL_Correção Loc Prod Emp.sql

---
+++ FL_Correção Loc Prod Emp.sql
@@ -0,0 +1,34 @@
+ALTER SESSION SET CURRENT_SCHEMA = CONSINCO;
+
+DECLARE 
+  i INTEGER := 0;
+  
+  BEGIN 
+    FOR t IN (
+
+    SELECT SEQPRODUTO, NROEMPRESA
+      FROM CONSINCO.MRL_PRODUTOEMPRESA A 
+     WHERE A.LOCENTRADA != (SELECT SEQLOCAL FROM CONSINCO.MRL_LOCAL L1 WHERE L1.LOCAL = 'LOJA' AND L1.NROEMPRESA = A.NROEMPRESA) AND A.NROEMPRESA < 100
+        OR A.LOCSAIDA   != (SELECT SEQLOCAL FROM CONSINCO.MRL_LOCAL L2 WHERE L2.LOCAL = 'LOJA' AND L2.NROEMPRESA = A.NROEMPRESA) AND A.NROEMPRESA < 100
+ 
+             )
+             
+     LOOP
+       BEGIN
+         i := 1+1;
+         UPDATE CONSINCO.MRL_PRODUTOEMPRESA X SET X.LOCENTRADA = (SELECT SEQLOCAL FROM CONSINCO.MRL_LOCAL L3 WHERE L3.LOCAL = 'LOJA' AND L3.NROEMPRESA = T.NROEMPRESA), -- Busca Seqlocal LOJA decada empresa
+                                                  X.LOCSAIDA   = (SELECT SEQLOCAL FROM CONSINCO.MRL_LOCAL L3 WHERE L3.LOCAL = 'LOJA' AND L3.NROEMPRESA = T.NROEMPRESA)  -- Busca Seqlocal LOJA decada empresa
+                                                  WHERE X.SEQPRODUTO = T.SEQPRODUTO
+                                                    AND X.NROEMPRESA = T.NROEMPRESA;
+                                                    
+         IF i = 10 THEN COMMIT;
+         i := 0;
+         END IF;
+         
+         EXCEPTION
+           WHEN OTHERS THEN
+             DBMS_OUTPUT.PUT_LINE('ERRO: '||T.SEQPRODUTO);
+         END;
+     END LOOP;
+    COMMIT;
+   END;
