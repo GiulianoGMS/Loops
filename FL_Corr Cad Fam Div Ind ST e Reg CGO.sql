491fd202ee5a32a334b2ffb60c2a7e1d636fe204
Giuliano Gomes <91477669+Giulianogms@users.noreply.github.com>
2023-10-10 10:05:33 -0300

    Create FL_Corr Cad Fam Div Ind ST e Reg CGO.sql

---
+++ FL_Corr Cad Fam Div Ind ST e Reg CGO.sql
@@ -0,0 +1,43 @@
+CREATE TABLE CONSINCO.NAGT_MAP_FAMDIVISAO_BKP_INDCGO AS
+
+SELECT F.SEQFAMILIA, F.INDMERCENQUADST, F.INDUSADADOSREGCGO
+
+  FROM  CONSINCO.MAP_FAMDIVISAO F
+                     INNER JOIN CONSINCO.MAP_FAMILIA Z ON F.SEQFAMILIA =Z.SEQFAMILIA
+
+ WHERE 1=1 
+   AND (NVL(F.INDMERCENQUADST, 'X') != 'S'
+    OR NVL(F.INDUSADADOSREGCGO, 'X') != 'S')
+    
+   --=================--
+   
+DECLARE
+      i INTEGER := 0;
+      
+      BEGIN
+        FOR t IN (SELECT F.SEQFAMILIA, F.INDMERCENQUADST, F.INDUSADADOSREGCGO
+                  FROM  CONSINCO.MAP_FAMDIVISAO F
+                                     INNER JOIN CONSINCO.MAP_FAMILIA Z ON F.SEQFAMILIA =Z.SEQFAMILIA
+
+                 WHERE 1=1 
+                   AND (NVL(F.INDMERCENQUADST, 'X') != 'S'
+                    OR NVL(F.INDUSADADOSREGCGO, 'X') != 'S')
+                    )
+       LOOP
+         BEGIN
+           i := i+1;
+       UPDATE CONSINCO.MAP_FAMDIVISAO X SET X.INDMERCENQUADST   = 'S',
+                                            X.INDUSADADOSREGCGO = 'S'
+                                     
+        WHERE 1=1 
+          AND T.SEQFAMILIA = X.SEQFAMILIA 
+          IF i = 10 THEN COMMIT;
+          i := 0;
+          END IF;
+          
+         END;
+       END LOOP;
+   COMMIT;
+ END;
+    
+    
