14996ba7d3c4b774da43b0a9f4bfd9b376a5419a
Giuliano Gomes <91477669+GiulianoGMS@users.noreply.github.com>
2025-01-22 17:04:59 -0300

    Create FL_Correção Cest com 6 Digitos.sql

---
+++ FL_Correção Cest com 6 Digitos.sql
@@ -0,0 +1,19 @@
+DECLARE i INTEGER := 0;
+-- Ticket 523216
+BEGIN
+  i := 1;
+  FOR t IN (SELECT SEQFAMILIA
+  FROM MAP_FAMILIA X WHERE LENGTH(X.CODCEST) = 6)
+  
+  LOOP
+    i := i+1;
+  UPDATE MAP_FAMILIA X SET X.CODCEST = RPAD(X.CODCEST,7,0),
+                           X.USUARIOALTERACAO = 'TKT523216',
+                           X.DTAHORALTERACAO = SYSDATE - 2
+                     WHERE X.SEQFAMILIA = T.SEQFAMILIA;
+    IF i = 25 THEN COMMIT;
+    i := 0;
+    END IF;
+  END LOOP;
+  COMMIT;
+END;
